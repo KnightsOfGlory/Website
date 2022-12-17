adf8a8fd7bc2f87f13b99a29942ddcf7c4bdcb8e
Richard Pianka <richardpianka@gmail.com>
2022-12-17 16:40:20 -0500

    feat(all): initial commit

---
+++ src/pages/Presentation/sections/Counters.js
@@ -0,0 +1,63 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+
+// Material Kit 2 React components
+import MKBox from "../../../components/MKBox";
+
+// Material Kit 2 React examples
+import DefaultCounterCard from "../../../examples/Cards/CounterCards/DefaultCounterCard";
+
+function Counters() {
+  return (
+    <MKBox component="section" py={3}>
+      <Container>
+        <Grid container item xs={12} lg={9} sx={{ mx: "auto" }}>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+                count={6601}
+                title="Downloads"
+                description="Across all of our projects and releases since the warnet reboot"
+            />
+          </Grid>
+          <Grid item xs={12} md={4} display="flex">
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, mx: 0 }} />
+            <DefaultCounterCard
+              count={8}
+              suffix="+"
+              title="Projects"
+              description="Software sponsored for the warnet community and others"
+            />
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, ml: 0 }} />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+                count={3}
+                // suffix="+"
+                title="Members"
+                description="We are gamers, programmers, and competitive warrers"
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+export default Counters;
---
+++ src/pages/Presentation/sections/Information.js
@@ -0,0 +1,107 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+// Material Kit 2 React components
+import MKBox from "../../../components/MKBox";
+
+// Material Kit 2 React examples
+import RotatingCard from "../../../examples/Cards/RotatingCard";
+import RotatingCardFront from "../../../examples/Cards/RotatingCard/RotatingCardFront";
+import RotatingCardBack from "../../../examples/Cards/RotatingCard/RotatingCardBack";
+import DefaultInfoCard from "../../../examples/Cards/InfoCards/DefaultInfoCard";
+
+// Images
+import bgFront from "../../../assets/images/rotating-card-bg-front.jpeg";
+import bgBack from "../../../assets/images/rotating-card-bg-back.jpeg";
+
+function Information() {
+  return (
+    <MKBox component="section" py={6} my={6}>
+      <Container>
+        <Grid container item xs={11} spacing={3} alignItems="center" sx={{ mx: "auto" }}>
+          <Grid item xs={12} lg={4} sx={{ mx: "auto" }}>
+            <RotatingCard>
+              <RotatingCardFront
+                image={bgFront}
+                icon="touch_app"
+                title={
+                  <>
+                    Learn About
+                    <br />
+                    What We Do
+                  </>
+                }
+                description={<div><br /><br /><br /></div>}
+              />
+              <RotatingCardBack
+                image={bgBack}
+                title="Apply to Join"
+                // description="Join an elite clan of gamers, developers, warrers, and casual chatters"
+                description={<span style={{textShadow: "2px 2px 4px #555"}}>
+                  Join an elite clan of gamers, developers, warrers, and casual chatters
+                </span>}
+                action={{
+                  type: "internal",
+                  route: "/sections/page-sections/page-headers",
+                  label: "start",
+                }}
+              />
+            </RotatingCard>
+          </Grid>
+          <Grid item xs={12} lg={7} sx={{ ml: "auto" }}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="sports_esports"
+                  title="Gaming"
+                  description="From Starcraft to Diablo to Warcraft, and everything in between, we love gaming"
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="code"
+                  title="Development"
+                  description="The world's most popular react components library for building user interfaces."
+                />
+              </Grid>
+            </Grid>
+            <Grid container spacing={3} sx={{ mt: { xs: 0, md: 6 } }}>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="local_fire_department"
+                  title="Warring"
+                  description="Creating your design from scratch with dedicated designers can be very expensive. Start with our Design System."
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="chat"
+                  title="Casual"
+                  description="Regardless of the screen size, the website content will naturally fit the given resolution."
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+export default Information;
